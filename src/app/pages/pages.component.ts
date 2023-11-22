5d26424f3f59b2d52c58c9700451ebd6b96f198b
rnairanjana <rnairanjana97@gmail.com>
2023-11-22 12:09:07 +0530

    first commit

---
+++ src/app/pages/pages.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pages',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pages.component.html',
+  styleUrl: './pages.component.css'
+})
+export class PagesComponent {
+
+}
---
+++ src/app/unit/unit.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-unit',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './unit.component.html',
+  styleUrl: './unit.component.css'
+})
+export class UnitComponent {
+
+}
